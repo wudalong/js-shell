829d351f29600489fed1a04980e1d97c520872d9
DeonWu <DeonWu@0bc048aa-f754-0410-aa17-7f98d66edaa7>
2008-08-24 08:01:03 +0000

    update doc

--- trunk/examples/jsdoc.js
+++ trunk/examples/jsdoc.js
@@ -254,7 +254,10 @@ function processJSFile(filename,inputdir)
 }
 
 function saveAsHtml(doc, out){
-    out.writeLine("<html><head><title>" + 
+    out.writeLine('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"')
+    out.writeLine('"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">')
+    out.writeLine('<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml">' +
+                  "<head><title>" + 
                   doc.file.o + 
                   "--doc</title>" +
                   "<link rel='stylesheet' href='screen.css' media='screen'>" +
