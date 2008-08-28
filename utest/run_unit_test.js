d6c4b4d5a2e24a37ed3218dd7b5cbf01500ea644
DeonWu <DeonWu@0bc048aa-f754-0410-aa17-7f98d66edaa7>
2008-08-28 09:33:11 +0000

    add licenses information

--- utest/run_unit_test.js
+++ utest/run_unit_test.js
@@ -1,5 +1,27 @@
 #!/cygdrive/c/USERS/_my_work/workspace/js-shell/build/bin/js -debug
 
+/* 
+ * Copyright (C) 2008 DeonWu@gmail.com
+ *
+ * This file is part of Js-Shell. Js-Shell is a set of library for running
+ * javascript in Rhino.
+ *
+ * Js-Shell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Js-Shell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Js-Shell.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $ Name LastChangeRevision LastChangeDate LastChangeBy $
+ * $Id$
+ */
 
 var file_lib = __import__('File', null, {})
 var ut = __import__('UnitTest', null, {})
