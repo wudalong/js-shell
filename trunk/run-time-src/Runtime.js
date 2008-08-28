b0bb56015b96a7b88f6f28807458e4c8f57b63ca
DeonWu <DeonWu@0bc048aa-f754-0410-aa17-7f98d66edaa7>
2008-08-28 09:33:11 +0000

    add licenses information

--- trunk/run-time-src/Runtime.js
+++ trunk/run-time-src/Runtime.js
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Js-Shell.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * $ Name LastChangeRevision LastChangeDate LastChangeBy $
  * $Id$
  */
 
