70852b3479f82099722bd43afea570bdb36d8832
DeonWu <DeonWu@0bc048aa-f754-0410-aa17-7f98d66edaa7>
2008-09-02 15:42:18 +0000

    init socket APIs

--- trunk/run-time-src/Socket.js
+++ trunk/run-time-src/Socket.js
@@ -22,20 +22,180 @@
  */
  
 
-var exports = ['SocketServer', 'SocketClient']
+var exports = ['SocketServer', 'Socket']
 
 /**
 *
 * @class SocketServer 
 */
-var SocketServer = function(){
-    
-}
+var SocketServer = extend(function(port){
+	    this.port = port
+	            
+	}, {
+	   
+	   accept: function(){
+	   },
+	   
+	   bind: function(address){
+	   },
+	   
+	   close: function(){
+	   },
+	   
+	   get localPort(){
+	   },
+	   
+	   get localAddress(){
+       },
+       
+       get timeout(){
+       },
+
+       set timeout(val){
+       },       
+	   
+	   /**
+	       add socket server event listener.
+	       *param:
+	       -type: (values: 'accept', 'connect')
+	       accept fn argument:(socketserver, socket)
+	       connect fn argument:(socketserver, socket)
+	   */
+	   addEventListener: function(type, fn){
+	   },
+	   
+       
+       /**
+            a short name of addEventListener
+       */   	   
+	   on: function(){
+	       this.addEventListener.apply(this, arguments)
+	   }
+	   
+       removeEventListener: function(type, fn){
+       },
+       
+       /**
+            a short name of removeEventListener
+       */       
+       un: function(){
+           this.removeEventListener.apply(this, arguments)
+       }
+	})
 
 /**
 *
-* @class SocketClient 
+* @class Socket 
 */
-var SocketClient = function(){
+var Socket = extend(function(addr, port){
+        this.port = port
+        this.blocking = true
+           
+    }, {
+       
+       bind: function(address){
+       },
+       
+       close: function(){
+       },
+       
+       /**
+        Connect to a remote socket at address.
+       */
+       connect: function(addr, port){
+       },
+       
+       get localPort(){
+       },
+       
+       get localAddress(){
+       },
+       
+       get port(){
+       
+       },
+       
+       get address(){
+       }, 
+       
+       get timeout(){
+       },
 
-}
+       set timeout(val){
+       },
+       
+       /**
+         java Socket object.
+       */
+       get socket() {
+       },
+       
+       /**
+         java Socket object.
+       */       
+       set socket() {
+       },
+          
+       /**
+           add socket server event listener.
+           *param:
+           -type: (values: 'read', 'write')
+           accept fn argument:(socket)
+           connect fn argument:(socket)
+       */
+       addEventListener: function(type, fn){
+       },
+       
+       /**
+            a short name of addEventListener
+       */
+       on: function(){
+           this.addEventListener.apply(this, arguments)
+       }
+       
+       removeEventListener: function(type, fn){
+       },
+       
+       /**
+            a short name of removeEventListener
+       */
+       un: function(){
+           this.removeEventListener.apply(this, arguments)
+       },
+       
+       /**
+            read specail length string. if the len is 0, read all data. 
+       */
+       read: function(len, block) {
+            
+       },
+       
+       /**
+            read a line. 
+       */
+       readLine: function(len, block) {
+            
+       },
+
+       /**
+            write data to socket. 
+       */
+       write: function(data) {
+       },
+
+       /**
+            write a line data to socket. 
+       */
+       writeLine: function(data){
+       },
+       
+       /**
+            Set blocking or non-blocking mode of the socket: if flag is 0, 
+            the socket is set to non-blocking, else to blocking mode. 
+            Initially all sockets are in blocking mode. set to non-blocking if
+            a 'read' type listener registered.
+       */
+       setblocking: function(block) {
+        
+       }   
+    })
